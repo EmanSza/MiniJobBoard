c1f8f24d4d973308fc9619289fe33e58deabf13c
EmanSza <eszabadka@hotmail.ca>
2026-03-20 02:07:59 -0400

    Added Additional Sanitization

--- server/src/Middleware/ValidateJobs.js
+++ server/src/Middleware/ValidateJobs.js
@@ -1,16 +1,27 @@
 import { body, validationResult, matchedData } from "express-validator";
-
+import sanitizeHtml from "sanitize-html";
 // TODO: Catch Categorys in Memory to validate Job posting?
+
+const stripAllHtml = (value) =>
+    sanitizeHtml(value, { allowedTags: [], allowedAttributes: {} });
 
 const validateJobPost = () => {
     return [
         body("title")
             .isString()
-            .isLength({ min: 1, max: 64 })
-            .withMessage("Title must be between1 and 64 characters")
+            .customSanitizer(stripAllHtml)
             .trim()
-            .escape(),
-        body("category").isString().withMessage("Invalid category"),
+            .escape()
+            .isLength({ min: 1, max: 64 })
+            .withMessage("Title must be between 1 and 64 characters"),
+
+        body("category")
+            .isString()
+            .customSanitizer(stripAllHtml)
+            .trim()
+            .escape()
+            .notEmpty()
+            .withMessage("Invalid category"),
         // requires isMongoId() when we implement categorys
         (req, res, next) => {
             const errors = validationResult(req);
